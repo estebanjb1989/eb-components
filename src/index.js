c47a9d38b356f7fab8260f15057c70872bcb1848
Esteban <esteban.fjb@proton.me>
2025-10-20 18:53:47 -0300

    fix/updated index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export { default as Button } from './Button.jsx'
 export { default as Header } from './components/Header'
 export { default as Page } from './components/Page'
+export { default as LandingPage } from './components/LandingPage'
+export { default as FeaturesSection } from './components/FeaturesSection'
+
